ee68074e9693d94ef61e6b7d7f30040c00747cdd
Apoorv Dhote <apoorvdhote2001@gmail.com>
2024-04-18 12:48:40 +0530

    first push

---
+++ app/(tabs)/four.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+import React from 'react';
+import { Text, View } from '@/components/Themed';
+import Chatbot from './src/Chatbot';
+
+export default function TabThreeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}></Text>
+      <Chatbot />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
